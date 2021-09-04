7daf56b596ada410fe4815ee797722883ccee2a5
Никита Бехтерев <fant560@gmail.com>
2021-09-04 23:53:48 +0300

    Пофиксил загрузку документов

--- backend_java/src/main/java/ru/psb/backend_java/repository/DocumentRepository.java
+++ backend_java/src/main/java/ru/psb/backend_java/repository/DocumentRepository.java
@@ -1,7 +1,9 @@
 package ru.psb.backend_java.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.psb.backend_java.entity.Document;
 
+@Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
 }
