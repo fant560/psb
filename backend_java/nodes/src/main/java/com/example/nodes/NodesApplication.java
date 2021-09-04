e0b8070d52fb6a41f739bef36216150474c0dc07
Никита Бехтерев <fant560@gmail.com>
2021-09-04 22:27:47 +0300

    Windows прекрати

---
+++ backend_java/nodes/src/main/java/com/example/nodes/NodesApplication.java
@@ -0,0 +1,12 @@
+package com.example.nodes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NodesApplication.class, args);
+    }
+}
