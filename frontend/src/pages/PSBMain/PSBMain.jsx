d060ec7ca44beaa120babaebb833a6a62c78a2e8
satori <diablomobile.modest@gmail.com>
2021-09-04 18:46:08 +0400

    Add submenus

--- frontend/src/pages/PSBMain/PSBMain.jsx
+++ frontend/src/pages/PSBMain/PSBMain.jsx
@@ -1,12 +1,13 @@
 import 'antd/dist/antd.css';
 import { Layout, Menu, Breadcrumb } from 'antd';
 import { contentStyle, mainPageSiderStyle, mainPageStyle } from "../Main/Main.styles"
-import React from "react"
+import React, { useState } from "react"
 import { FileSyncOutlined, UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons'
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
 const PSBMain = () => {
+    useState()
     return (
         <Layout>
             <Sider collapsible >
@@ -20,11 +21,13 @@ const PSBMain = () => {
                         <Menu.Item key="1">Документ</Menu.Item>
                         <Menu.Item key="2">Архив</Menu.Item>
                     </SubMenu>
-                    <SubMenu key="sub2" icon={<LaptopOutlined />} title="Номенклатура">
-                        <Menu.Item key="5">option5</Menu.Item>
-                        <Menu.Item key="6">option6</Menu.Item>
-                        <Menu.Item key="7">option7</Menu.Item>
-                        <Menu.Item key="8">option8</Menu.Item>
+                    <SubMenu key="sub2" icon={<LaptopOutlined />} title="Периоды">
+                        <Menu.Item key="5">1 полугодие 2021</Menu.Item>
+                        <Menu.Item key="6">2 полугодие 2020</Menu.Item>
+                    </SubMenu>
+                    <SubMenu key="sub3" icon={<LaptopOutlined />} title="Отчеты">
+                        <Menu.Item key="7">1 полугодие 2021</Menu.Item>
+                        <Menu.Item key="8">2 полугодие 2020</Menu.Item>
                     </SubMenu>
                 </Menu>
             </Sider>
