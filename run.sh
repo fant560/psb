f2d4a21abd1b978f6644ee2acfa6a654b68dea1b
Никита Бехтерев <fant560@gmail.com>
2021-09-05 03:05:58 +0300

    Пофиксил скрипт запуска

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pushd backend_java && ./mvnw clean install -DkipTests && popd \
+pushd backend_java && ./mvnw clean install -DkipTests && pushd nodes \
+&& ./mvnw clean install -DkipTests && popd && popd \
 && pushd frontend && yarn && docker-compose up --build
